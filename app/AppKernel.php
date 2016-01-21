665dfd0ec52edaa2015a78730f66b8a11bed2185
Dawid Labno <costamcos@dodsi.com>
2016-01-21 21:21:00 +0300

    Dodanie odpowiednich modulow bootstrapa

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Lexik\Bundle\FormFilterBundle\LexikFormFilterBundle(),
+            new JordiLlonch\Bundle\CrudGeneratorBundle\JordiLlonchCrudGeneratorBundle(),
             new AppBundle\AppBundle(),
         );
 
